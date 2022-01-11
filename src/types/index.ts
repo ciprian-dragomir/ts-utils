b956c3f94d99c65ec1a8e91ed6e72f8328e2b682
ciprian <ciprian.dragomir@icloud.com>
2022-01-11 08:15:07 +0000

    Add maybe fn and doc comments.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,20 @@
 export type Maybe<T> = T | undefined;
 export type AnyObject = Record<string, unknown>;
+
+/**
+ * Monadic bind for the Maybe monad.
+ * @param a
+ * @param b
+ */
 export const flatMap = <T, P>(a: Maybe<T>, b: (c: T) => Maybe<P>): Maybe<P> =>
   a === undefined ? undefined : b(a);
 
-export type Optional<T extends AnyObject, Q extends keyof T> = Omit<T, Q> &
-  {
-    [key in Q]+?: T[key];
-  };
+/**
+ * Convert a nullable value to a Maybe monad (null is translated to undefined).
+ * @param a
+ */
+export const maybe = <T>(a: T | null): Maybe<T> => a || undefined;
+
+export type Optional<T extends AnyObject, Q extends keyof T> = Omit<T, Q> & {
+  [key in Q]+?: T[key];
+};
