10378149940efa8cbcff1e3321711215188c1e4e
ciprian <ciprian.dragomir@icloud.com>
2022-01-29 17:15:19 +0000

    Add equals sugar.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './types';
 export * from './splitArray';
 export * from './range';
+
+export const equals =
+  <T>(a: T, isEqual: (x: T, y: T) => boolean = Object.is) =>
+  (b: T) =>
+    isEqual(a, b);
