6fd21e64f7e05ae2c1096a61e9e0c30c59043eb2
ciprian <ciprian.dragomir@icloud.com>
2022-03-16 13:24:31 +0000

    Add logValue

--- src/misc/index.ts
+++ src/misc/index.ts
@@ -1,1 +1,8 @@
 export const noop: () => void = () => {};
+
+export const logValue = <T>(value: T, message?: string, logFn = console.log): T => {
+  if (logFn) {
+    message ? logFn(message, value) : logFn(value);
+  }
+  return value;
+};
