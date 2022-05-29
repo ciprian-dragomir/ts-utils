83f667d4c3ed95e8086e816120b7454f869e058a
ciprian <ciprian.dragomir@icloud.com>
2022-05-29 10:59:34 +0100

    Change migration format.

--- src/localStorage/index.ts
+++ src/localStorage/index.ts
@@ -10,48 +10,88 @@ export type LocalStorageError<T extends AnyObject, K extends keyof T> =
 export type LocalStorageFactoryParams<T extends AnyObject> = {
   storage?: Pick<typeof window.localStorage, 'getItem' | 'setItem' | 'clear'>;
   onError?: <K extends keyof T = keyof T>(info: LocalStorageError<T, K>) => void;
+  migrations?: Partial<{ [K in keyof T]: () => { value: T[K]; onComplete?: () => void } }>;
 };
 
-const DefaultParams: Partial<LocalStorageFactoryParams<AnyObject>> = {
+const getDefaultParams = <T extends AnyObject>(): Partial<LocalStorageFactoryParams<T>> => ({
   storage: globalThis?.localStorage,
+});
+
+export type LocalStorage<T extends AnyObject> = {
+  setItem: <K extends keyof T>(
+    key: K,
+    value: T[K],
+    onErrorSpecific?: LocalStorageFactoryParams<T>['onError'],
+  ) => boolean;
+  getItemOrDefault: <K extends keyof T, P = T[K]>(
+    key: K,
+    defaultReturn: P,
+    onErrorSpecific?: LocalStorageFactoryParams<T>['onError'],
+  ) => T[K] | P;
+  getItem: <K extends keyof T>(
+    key: K,
+    onErrorSpecific?: LocalStorageFactoryParams<T>['onError'],
+  ) => Maybe<T[K]>;
+  clear: () => boolean;
 };
 
 const localStorageFactory = <T extends AnyObject>(params?: LocalStorageFactoryParams<T>) => {
-  const { storage, onError } = { ...DefaultParams, ...params };
+  const { storage, onError, migrations } = { ...getDefaultParams<T>(), ...params };
   if (!storage) {
     throw Error('A valid "storage" must be specified.');
   }
 
-  const setItem = <K extends keyof T>(key: K, value: T[K]) => {
+  const setItem: LocalStorage<T>['setItem'] = (key, value, onErrorSpecific) => {
     const item = { value };
     try {
       storage.setItem(key.toString(), JSON.stringify(item));
+      return true;
     } catch (error) {
-      onError?.({ key, op: 'set', value, error });
+      (onErrorSpecific || onError)?.({ key, op: 'set', value, error });
     }
+
+    return false;
   };
 
-  const getItemOrDefault = <K extends keyof T, P = T[K]>(key: K, defaultReturn: P): T[K] | P => {
+  const getItemOrDefault: LocalStorage<T>['getItemOrDefault'] = (
+    key,
+    defaultReturn,
+    onErrorSpecific,
+  ) => {
     try {
-      const item = storage.getItem(key.toString());
+      const k = key.toString();
+      const item = storage.getItem(k);
       if (item) {
         return JSON.parse(item).value || defaultReturn;
+      } else {
+        const migration = migrations?.[k]?.();
+        if (migration) {
+          const { value, onComplete } = migration;
+          if (setItem(k, migration.value)) {
+            onComplete?.();
+          }
+          return value;
+        }
       }
     } catch (error) {
-      onError?.({ key, op: 'get', error });
+      (onErrorSpecific || onError)?.({ key, op: 'get', error });
     }
 
     return defaultReturn;
   };
 
-  const getItem = <K extends keyof T>(key: K): Maybe<T[K]> => getItemOrDefault(key, undefined);
+  const getItem: LocalStorage<T>['getItem'] = (key, onErrorSpecific) =>
+    getItemOrDefault(key, undefined, onErrorSpecific);
 
   const clear = () => {
     try {
       storage.clear();
+      return true;
     } catch (error) {
       onError?.({ op: 'clear' });
     }
+
+    return false;
   };
 
   return { setItem, getItem, getItemOrDefault, clear };
